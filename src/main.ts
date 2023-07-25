da6f3b0800a2845d56fe06a262b7f984c7d928af
Danylo Zalizchuk <danilmail0110@gmail.com>
2023-07-25 21:31:09 +0300

    feat: add Vercel Analytics

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
+import { inject } from '@vercel/analytics';
+
 import App from './App.vue';
 import router from './router';
 
@@ -38,6 +40,7 @@ import 'primeicons/primeicons.css';
 import 'primeflex/primeflex.css';
 import './assets/main.css';
 
+inject(); // Vercel Analytics
 const app = createApp(App);
 
 app.use(createPinia());
