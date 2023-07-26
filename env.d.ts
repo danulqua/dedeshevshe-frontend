39de99ffefd45a20f8a3502b3113c0dda8849b5f
Danylo Zalizchuk <danilmail0110@gmail.com>
2023-07-26 17:56:56 +0300

    feat: add donation url

--- env.d.ts
+++ env.d.ts
@@ -3,6 +3,7 @@
 interface ImportMetaEnv {
   readonly VITE_API_BASE_URL: string;
   readonly VITE_EMAIL_FOR_FEEDBACK: string;
+  readonly VITE_DONATION_URL: string;
 }
 
 interface ImportMeta {
--- src/components/MainFooter.vue
+++ src/components/MainFooter.vue
@@ -1,9 +1,15 @@
 <template>
   <footer class="p-4 text-500">
     <div class="flex flex-column align-items-center gap-2">
-      <a :href="`mailto:${feedbackEmail}`" class="text-center text-500 underline">
-        Зворотній звʼязок
-      </a>
+      <div class="flex gap-2">
+        <a :href="`mailto:${feedbackEmail}`" class="text-center text-500 underline">
+          Зворотній звʼязок
+        </a>
+        |
+        <a :href="donationUrl" class="text-center text-500 underline" target="_blank">
+          Підтримати проєкт
+        </a>
+      </div>
       <div class="flex justify-content-center align-items-center gap-2">
         <span>&copy; {{ year }}</span>
         <span>Всі права захищені.</span>
@@ -17,6 +23,7 @@ import { ref } from 'vue';
 
 const year = ref(new Date().getFullYear());
 const feedbackEmail = import.meta.env.VITE_EMAIL_FOR_FEEDBACK;
+const donationUrl = import.meta.env.VITE_DONATION_URL;
 </script>
 
 <style scoped></style>
