48e65d2a191221c79fbd5fc2b4e532e8cfbead4a
Danylo Zalizchuk <danilmail0110@gmail.com>
2023-07-26 16:56:19 +0300

    feat: add footer

--- env.d.ts
+++ env.d.ts
@@ -2,6 +2,7 @@
 
 interface ImportMetaEnv {
   readonly VITE_API_BASE_URL: string;
+  readonly VITE_EMAIL_FOR_FEEDBACK: string;
 }
 
 interface ImportMeta {
--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,11 +1,25 @@
 <template>
-  <MainHeader />
+  <div class="main-layout">
+    <MainHeader />
 
-  <main class="my-6 container">
-    <RouterView />
-  </main>
+    <main class="my-6 container">
+      <RouterView />
+    </main>
+
+    <MainFooter />
+  </div>
 </template>
 
 <script setup lang="ts">
 import MainHeader from '@/components/MainHeader.vue';
+import MainFooter from '@/components/MainFooter.vue';
 </script>
+
+<style scoped lang="scss">
+.main-layout {
+  display: grid;
+  grid-template-rows: auto 1fr auto;
+  grid-template-columns: 1fr;
+  min-height: 100%;
+}
+</style>
